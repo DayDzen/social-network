d9d9c7b75ceb72f1f0571e506af3d58eac6581f4
Izmaylov Ilia <ilizmaylov@ozon.ru>
2022-04-16 18:05:14 +0300

    Created some handlers

--- main.go
+++ main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"text/template"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
 
-func dbConn() (db *sql.DB) {
+type SEX string
+
+const (
+	Male   SEX = "Male"
+	Female SEX = "Female"
+	EMPTY  SEX = ""
+)
+
+type UserProfile struct {
+	ID        int64
+	FirstName string
+	LastName  string
+	Age       int16
+	Sex       SEX
+	Hobbies   string
+	City      string
+}
+
+func connectToDB() *sql.DB {
 	dbDriver := "mysql"
 	dbUser := os.Getenv("DB_USERNAME")
 	dbPass := os.Getenv("DB_PASSWORD")
@@ -22,14 +42,113 @@ func dbConn() (db *sql.DB) {
 	return db
 }
 
+var tmpl *template.Template
+
 func init() {
+	log.Println("INIT Started")
+
 	if err := godotenv.Load("prod_config.yaml"); err != nil {
 		if err = godotenv.Load("local_config.yaml"); err != nil {
 			log.Print("No config file found")
 		}
 	}
+
+	tmpl = template.Must(template.ParseGlob("form/*"))
+	log.Println("SUCCESS init template")
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	dbConn := connectToDB()
+	defer dbConn.Close()
+
+	selDB, err := dbConn.Query("SELECT * FROM user_profiles ORDER BY id DESC")
+	if err != nil {
+		panic(err.Error())
+	}
+	uProfile := UserProfile{}
+	res := []UserProfile{}
+	for selDB.Next() {
+		var id int64
+		var age int16
+		var fName, lName, sex, hobbies, city string
+		err = selDB.Scan(&id, &fName, &lName, &age, &sex, &hobbies, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+		uProfile.ID = id
+		uProfile.FirstName = fName
+		uProfile.LastName = lName
+		uProfile.Age = age
+		uProfile.Sex = SEX(sex)
+		uProfile.Hobbies = hobbies
+		uProfile.City = city
+
+		res = append(res, uProfile)
+	}
+	tmpl.ExecuteTemplate(w, "Index", res)
+}
+
+func Show(w http.ResponseWriter, r *http.Request) {
+	dbConn := connectToDB()
+	defer dbConn.Close()
+
+	nId := r.URL.Query().Get("id")
+	selDB, err := dbConn.Query("SELECT * FROM user_profiles WHERE id=?", nId)
+	if err != nil {
+		panic(err.Error())
+	}
+	uProfile := UserProfile{}
+	for selDB.Next() {
+		var id int64
+		var age int16
+		var fName, lName, sex, hobbies, city string
+		err = selDB.Scan(&id, &fName, &lName, &age, &sex, &hobbies, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		uProfile.ID = id
+		uProfile.FirstName = fName
+		uProfile.LastName = lName
+		uProfile.Age = age
+		uProfile.Sex = SEX(sex)
+		uProfile.Hobbies = hobbies
+		uProfile.City = city
+	}
+	tmpl.ExecuteTemplate(w, "Show", uProfile)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	dbConn := connectToDB()
+	defer dbConn.Close()
+
+	if r.Method == "POST" {
+		fName := r.FormValue("f_name")
+		lName := r.FormValue("l_name")
+		age := r.FormValue("age")
+		sex := r.FormValue("sex")
+		hobbies := r.FormValue("hobbies")
+		city := r.FormValue("city")
+		insForm, err := dbConn.Prepare("INSERT INTO user_profiles(f_name, l_name, age, sex, hobbies, city) VALUES(?,?,?,?,?,?)")
+		if err != nil {
+			panic(err.Error())
+		}
+		insForm.Exec(fName, lName, city)
+		log.Printf("INSERT:\nfName: %v\nlName: %v\nAge: %v\nSex: %v\nHobbies: %v\nCity: %v\n", fName, lName, age, sex, hobbies, city)
+	}
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
 
 func main() {
-	fmt.Print("Hello")
+	log.Println("Server started on: http://localhost:8080")
+
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/show", Show)
+	http.HandleFunc("/edit", Insert)
+
+	http.ListenAndServe(":8080", nil)
 }
