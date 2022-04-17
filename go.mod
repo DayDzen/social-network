5e9b38a3f20f9ff801e1b0cb7600fe81987b2e1e
Izmaylov Ilia <ilizmaylov@ozon.ru>
2022-04-17 16:52:48 +0300

    v0.0.3

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.17
 require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/joho/godotenv v1.4.0
+	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064
 )
--- main.go
+++ main.go
@@ -10,14 +10,15 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
 )
 
-type SEX string
+type GENDER string
 
 const (
-	Male   SEX = "Male"
-	Female SEX = "Female"
-	EMPTY  SEX = ""
+	Male   GENDER = "Male"
+	Female GENDER = "Female"
+	EMPTY  GENDER = ""
 )
 
 type User struct {
@@ -25,7 +26,7 @@ type User struct {
 	FirstName string
 	LastName  string
 	Age       int16
-	Sex       SEX
+	Gender    GENDER
 	Hobbies   string
 	City      string
 }
@@ -81,10 +82,7 @@ func init() {
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	// dbConn := connectToDB()
-	// defer dbConn.Close()
-
-	selDB, err := dbConn.Query("SELECT * FROM user_profiles ORDER BY id DESC")
+	selDB, err := dbConn.Query("SELECT id, first_name, last_name FROM users ORDER BY id DESC")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -92,50 +90,81 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	res := []User{}
 	for selDB.Next() {
 		var id int64
-		var age int16
-		var fName, lName, sex, hobbies, city string
-		err = selDB.Scan(&id, &fName, &lName, &age, &sex, &hobbies, &city)
+		var fName, lName string
+		err = selDB.Scan(&id, &fName, &lName)
+		// var age int16
+		// var gender, hobbies, city string
+		// err = selDB.Scan(&id, &fName, &lName, &age, &gender, &hobbies, &city)
 		if err != nil {
 			panic(err.Error())
 		}
 		user.ID = id
 		user.FirstName = fName
 		user.LastName = lName
-		user.Age = age
-		user.Sex = SEX(sex)
-		user.Hobbies = hobbies
-		user.City = city
+		// user.Age = age
+		// user.Gender = GENDER(gender)
+		// user.Hobbies = hobbies
+		// user.City = city
 
 		res = append(res, user)
 	}
-	log.Println(res)
 	tmpl.ExecuteTemplate(w, "Index", res)
 }
 
-func Show(w http.ResponseWriter, r *http.Request) {
-	// dbConn := connectToDB()
-	// defer dbConn.Close()
+func SignUp(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		login := r.FormValue("login")
+		pass := getHash([]byte(r.FormValue("pass")))
 
+		fName := r.FormValue("first_name")
+		lName := r.FormValue("last_name")
+		age := r.FormValue("age")
+		gender := r.FormValue("gender")
+		hobbies := r.FormValue("hobbies")
+		city := r.FormValue("city")
+
+		insForm, err := dbConn.Prepare("INSERT INTO users(login, password, first_name, last_name, age, gender, hobbies, city) VALUES(?,?,?,?,?,?,?,?)")
+		if err != nil {
+			err = fmt.Errorf("SignUp err: %w", err)
+			panic(err.Error())
+		}
+
+		_, err = insForm.Exec(login, pass, fName, lName, age, gender, hobbies, city)
+		if err != nil {
+			err = fmt.Errorf("SignUp err: %w", err)
+			panic(err.Error())
+		}
+		log.Printf("INSERT:\nFirst Name: %v\nLast Name: %v\nAge: %v\nGender: %v\nHobbies: %v\nCity: %v\n", fName, lName, age, gender, hobbies, city)
+	}
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+}
+
+func getHash(pwd []byte) string {
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+	return string(hash)
+}
+
+func Show(w http.ResponseWriter, r *http.Request) {
 	nId := r.URL.Query().Get("id")
-	selDB, err := dbConn.Query("SELECT * FROM user_profiles WHERE id=?", nId)
+	selDB, err := dbConn.Query("SELECT first_name, last_name, age, gender, hobbies, city FROM users WHERE id=?", nId)
 	if err != nil {
 		panic(err.Error())
 	}
 	user := User{}
 	for selDB.Next() {
-		var id int64
 		var age int16
-		var fName, lName, sex, hobbies, city string
-		err = selDB.Scan(&id, &fName, &lName, &age, &sex, &hobbies, &city)
+		var fName, lName, gender, hobbies, city string
+		err = selDB.Scan(&fName, &lName, &age, &gender, &hobbies, &city)
 		if err != nil {
 			panic(err.Error())
 		}
-
-		user.ID = id
 		user.FirstName = fName
 		user.LastName = lName
 		user.Age = age
-		user.Sex = SEX(sex)
+		user.Gender = GENDER(gender)
 		user.Hobbies = hobbies
 		user.City = city
 	}
@@ -146,34 +175,37 @@ func New(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "New", nil)
 }
 
-func Insert(w http.ResponseWriter, r *http.Request) {
-	// dbConn := connectToDB()
-	// defer dbConn.Close()
+// func Insert(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method == "POST" {
+// 		fName := r.FormValue("first_name")
+// 		lName := r.FormValue("last_name")
+// 		age := r.FormValue("age")
+// 		gender := r.FormValue("gender")
+// 		hobbies := r.FormValue("hobbies")
+// 		city := r.FormValue("city")
 
-	if r.Method == "POST" {
-		fName := r.FormValue("first_name")
-		lName := r.FormValue("last_name")
-		age := r.FormValue("age")
-		sex := r.FormValue("sex")
-		hobbies := r.FormValue("hobbies")
-		city := r.FormValue("city")
-		insForm, err := dbConn.Prepare("INSERT INTO user_profiles(first_name, last_name, age, sex, hobbies, city) VALUES(?,?,?,?,?,?)")
-		if err != nil {
-			panic(err.Error())
-		}
-		insForm.Exec(fName, lName, age, sex, hobbies, city)
-		log.Printf("INSERT:\nFirst Name: %v\nLast Name: %v\nAge: %v\nSex: %v\nHobbies: %v\nCity: %v\n", fName, lName, age, sex, hobbies, city)
-	}
-	http.Redirect(w, r, "/", http.StatusMovedPermanently)
-}
+// 		insForm, err := dbConn.Prepare("INSERT INTO users(first_name, last_name, age, gender, hobbies, city) VALUES(?,?,?,?,?,?)")
+// 		if err != nil {
+// 			panic(err.Error())
+// 		}
+// 		insForm.Exec(fName, lName, age, gender, hobbies, city)
+// 		log.Printf("INSERT:\nFirst Name: %v\nLast Name: %v\nAge: %v\nGender: %v\nHobbies: %v\nCity: %v\n", fName, lName, age, gender, hobbies, city)
+// 	}
+// 	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+// }
 
 func main() {
 	log.Println("Server started on: http://localhost:8080")
 
 	http.HandleFunc("/", Index)
+
+	http.HandleFunc("/sign-up", SignUp)
+	// http.HandleFunc("/sign-in", SignIn)
+	// http.HandleFunc("/sign-offss", SignOff)
+
 	http.HandleFunc("/show", Show)
 	http.HandleFunc("/new", New)
-	http.HandleFunc("/insert", Insert)
+	// http.HandleFunc("/insert", Insert)
 
 	http.ListenAndServe(":8080", nil)
 }
